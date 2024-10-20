433c720a0573008a1a6132089fc5636b51740651
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-20 18:11:21 +0300

    Rotate player using mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 	[SerializeField] private CharacterController _characterController;
+	[SerializeField] private Transform _view;
 	[SerializeField] private float _speed = 1.0f;
+	[SerializeField] private float _sensitivity = 1.0f;
 
-	private Vector3 _velocity;
-
-	private Vector3 _wishDir;
+	private Vector2 _wishDir;
+	private Vector2 _deltaMouse;
 
 	private void FixedUpdate()
 	{
-		_velocity = _wishDir * _speed;
-
-		// Process movement
-		_characterController.SimpleMove(_velocity);
+		ProcessMovement();
 	}
 
-	// Update wish move direction and rotation each tick
 	private void Update()
 	{
-		_wishDir = new Vector3(
+		UpdateUserInput();
+		ProcessViewing();
+	}
+
+	private void ProcessMovement()
+	{
+		var worldWishDir = new Vector3(_wishDir.x, 0, _wishDir.y);
+		worldWishDir.Normalize();
+
+		var deltaRot = worldWishDir * _speed;
+		var moveDir = Quaternion.AngleAxis(transform.rotation.eulerAngles.y, Vector3.up) * deltaRot;
+
+		_characterController.SimpleMove(moveDir);
+	}
+
+	private void ProcessViewing()
+	{
+		var deltaRot = _deltaMouse * _sensitivity;
+		deltaRot.y = -deltaRot.y;
+
+		transform.Rotate(Vector3.up, deltaRot.x);
+		_view.Rotate(Vector3.right, deltaRot.y);
+	}
+
+	private void UpdateUserInput()
+	{
+		_wishDir = new Vector2(
 			Input.GetAxis("Horizontal"),
-			0,
 			Input.GetAxis("Vertical"));
+
+		_deltaMouse = new Vector2(
+			Input.GetAxis("Mouse X"),
+			Input.GetAxis("Mouse Y"));
 	}
 }
