64a1e6fe3bdca3deab2bef6d3a2728e842be99cb
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-21 00:44:21 +0300

    Remove unused references

--- Assets/Scripts/Truck.cs
+++ Assets/Scripts/Truck.cs
@@ -1,8 +1,6 @@
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Truck : MonoBehaviour
 {
