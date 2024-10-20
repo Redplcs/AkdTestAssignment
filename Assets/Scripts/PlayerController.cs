a48d37c40c8f08a4118f4adbd7b5845267aa797e
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-20 17:12:07 +0300

    Store user input and velocity as private fields

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,24 @@ public class PlayerController : MonoBehaviour
 	[SerializeField] private CharacterController _characterController;
 	[SerializeField] private float _speed = 1.0f;
 
+	private Vector3 _velocity;
+
+	private Vector3 _wishDir;
+
 	private void FixedUpdate()
 	{
-		var wishDir = new Vector3(
+		_velocity = _wishDir * _speed;
+
+		// Process movement
+		_characterController.SimpleMove(_velocity);
+	}
+
+	// Update wish move direction and rotation each tick
+	private void Update()
+	{
+		_wishDir = new Vector3(
 			Input.GetAxis("Horizontal"),
 			0,
 			Input.GetAxis("Vertical"));
-
-		_characterController.SimpleMove(_speed * wishDir);
 	}
 }
